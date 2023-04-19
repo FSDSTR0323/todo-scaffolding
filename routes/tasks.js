18659ad80789805057b31f516a0a19f3a4914a92
Jordi Galobart Salvat <jgalobart@gmail.com>
2023-04-19 20:47:42 +0200

    add tasks info

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,7 +6,7 @@ const tasks = [
         id: 1,
         task: "Preparar la clase de Nuclio",
         dueDate: "2023/04/20 18:30:00",
-        status: "PENDING",
+        status: "COMPLETED",
         createdAt: "2023/04/15 18:30:00",
         modifiedAt: null,
         deletedAt: null,
@@ -14,21 +14,46 @@ const tasks = [
     {
         id: 2,
         task: "Preparar las maletas",
-        dueDate: "2023/07/20"
+        dueDate: "2023/07/20",
+        status: "PENDING",
+        createdAt: "2023/04/15 18:30:00",
+        modifiedAt: null,
+        deletedAt: null,
     },
     {
         id: 3,
         task: "Celebrar fin de año",
-        dueDate: "2023/12/31"
+        dueDate: "2023/12/31",
+        status: "PENDING",
+        createdAt: "2023/04/15 18:30:00",
+        modifiedAt: null,
+        deletedAt: null,
     },
     {
         id: 4,
         task: "Celebrar la navidad",
-        dueDate: "2023/12/25"
+        dueDate: "2023/12/25",
+        status: "PENDING",
+        createdAt: "2023/04/15 18:30:00",
+        modifiedAt: "2023/04/18 11:15:00",
+        deletedAt: null,
+    }
+    ,
+    {
+        id: 5,
+        task: "TEST",
+        dueDate: "2023/12/25",
+        status: "PENDING",
+        createdAt: "2023/04/15 18:30:00",
+        modifiedAt: null,
+        deletedAt: "2023/04/15 19:30:00",
     }
 ]
 
-router.get('/', (req, res) => {
+router.get('/:id?', (req, res) => {
+
+    if (req.params.color!==null) {}
+
     res.send(tasks)
   })
 
